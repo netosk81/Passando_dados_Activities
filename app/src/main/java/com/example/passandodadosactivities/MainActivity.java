21e237ed48d2ed802cf4df67e8b5334f9d525079
netosk81 <hk.neto@gmail.com>
2023-04-27 21:20:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/passandodadosactivities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.passandodadosactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
